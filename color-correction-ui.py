5860136ffbde2949a682b8db1dcb2043c81daf1d
John D <john@johnd.ninja>
2018-08-09 21:48:29 -0700

    Navarro 2018 on-site field updates

--- color-correction-ui.py
+++ color-correction-ui.py
@@ -29,19 +29,21 @@ def update(_):
             blue.get(),
         ])
 
-def slider(name, from_, to):
+def slider(name, from_, to, setpoint):
     s = tk.Scale(root, label=name, from_=from_, to=to, resolution=0.01,
         showvalue='yes', orient='horizontal', length=400, command=update)
-    s.set(1.0)
+    s.set(setpoint)
     s.pack()
     return s
+
+config = json.load(open('./fcserver/temple_new.json'))
 
 root = tk.Tk()
 root.title("Fadecandy Color Correction Example")
 
-gamma = slider("Gamma", 0.2, 3.0)
-red = slider("Red", 0.0, 1.5)
-green = slider("Green", 0.0, 1.5)
-blue = slider("Blue", 0.0, 1.5)
+gamma = slider("Gamma", 0.2, 3.0, config["color"]["gamma"])
+red = slider("Red", 0.0, 1.5, config["color"]["whitepoint"][0])
+green = slider("Green", 0.0, 1.5, config["color"]["whitepoint"][1])
+blue = slider("Blue", 0.0, 1.5, config["color"]["whitepoint"][2])
 
 root.mainloop()
